90f312752b0ecc31672059e45304062632c48f69
Bütler Tim Philipp <butlt2@students.bfh.ch>
2024-11-12 13:06:41 +0100

    removed FXML and implemented the JavaFX inside of BoardMain.java

--- board/src/main/java/ch/bfh/akka/botrace/board/gui/BoardMain.java
+++ board/src/main/java/ch/bfh/akka/botrace/board/gui/BoardMain.java
@@ -7,13 +7,10 @@ import akka.actor.ActorSystem;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -30,8 +27,6 @@ public class BoardMain extends Application {
 	private Button stop;
 	@FXML
 	private GridPane gamefield;
-
-	private ActorSystem actorSystem;
 
 	/**
 	 * Initialize the actor system.
@@ -50,29 +45,77 @@ public class BoardMain extends Application {
 		// TODO Replace the following by a 'real' GUI
 		//Label label = new Label("Hello Akka programmer, please complete the GUI for the board");
 
-		// Load the FXML file
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("BoardMain.fxml"));
-		if (loader.getLocation() == null) {
-			throw new IllegalStateException("FXML file not found");
-		}
-		Parent root = loader.load();
+		// Create the root layout
+		BorderPane root = new BorderPane();
 
+		// Create the VBox for bot list and controls
+		botList = new VBox();
+		botList.setPrefHeight(91.0);
+		botList.setPrefWidth(600.0);
+
+		// Create a ChoiceBox for speed
+		speed = new ChoiceBox<>();
+		speed.getItems().addAll(200, 500, 2000, 5000);
+		speed.setValue(speed.getItems().getFirst());
+		speed.setPrefWidth(150.0);
+
+		// Create Start and Stop buttons
+		start = new Button("Start");
+		start.setOnAction(event -> startRace());
+
+		stop = new Button("Stop");
+		stop.setOnAction(event -> stopRace());
+
+		// Add the ChoiceBox and buttons to the left side (VBox)
+		VBox leftPanel = new VBox(speed, start, stop);
+		leftPanel.setPrefWidth(100.0);
+
+		// Create the game field (GridPane)
+		gamefield = new GridPane();
+		gamefield.setPrefWidth(400.0);
+		gamefield.setPrefHeight(200.0);
+
+		// Configure grid column constraints
+		ColumnConstraints col1 = new ColumnConstraints();
+		col1.setHgrow(javafx.scene.layout.Priority.SOMETIMES);
+		col1.setMinWidth(10.0);
+		col1.setPrefWidth(100.0);
+
+		ColumnConstraints col2 = new ColumnConstraints();
+		col2.setHgrow(javafx.scene.layout.Priority.SOMETIMES);
+		col2.setMinWidth(10.0);
+		col2.setPrefWidth(100.0);
+
+		gamefield.getColumnConstraints().addAll(col1, col2);
+
+		// Configure grid row constraints
+		RowConstraints row1 = new RowConstraints();
+		row1.setVgrow(javafx.scene.layout.Priority.SOMETIMES);
+		row1.setMinHeight(10.0);
+		row1.setPrefHeight(30.0);
+
+		RowConstraints row2 = new RowConstraints();
+		row2.setVgrow(javafx.scene.layout.Priority.SOMETIMES);
+		row2.setMinHeight(10.0);
+		row2.setPrefHeight(30.0);
+
+		gamefield.getRowConstraints().addAll(row1, row2);
+
+		// Set the components into the BorderPane layout
+		root.setTop(botList);
+		root.setLeft(leftPanel);
+		root.setCenter(gamefield);
 
 		// set up the GUI
 		Scene scene = new Scene(root, 500, 200);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Board of Group 06");
 		primaryStage.show();
-
-		// set up the event handlers
-		start.setOnAction(event -> startRace());
-		stop.setOnAction(event -> stopRace());
 	}
 
 	@Override
 	public void stop() {
 		// TODO Terminate actor system
-		actorSystem.terminate();
 		Platform.exit();
 	}
 
@@ -81,7 +124,7 @@ public class BoardMain extends Application {
 	}
 
 	public void startRace() {
-		System.out.println("Race started.");
+		System.out.println("Race started.\nSpeed: " + speed.getValue());
 	}
 
 	public void stopRace() {
